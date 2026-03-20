e238725b2a65040ef1ce9c1abc4b48a92957a07b
lenowski <stanislaw.len@proton.me>
2026-03-20 14:34:22 +0100

    refactor!: convert crate to RigidBody2D
    Switch crate to RigidBody2D to rely on built-in sleeping and physics
    behavior, reducing manual per-frame updates and enabling natural
    inter-crate interactions.
    
    * Use engine sleeping/wake instead of manual gravity and MoveAndSlide
    * Wake up nearby crates when one shatters to propagate physics
    * Replace Gravity with ShatterVelocityThreshold to control break
      velocity
    * Move nodes to top-level RigidBody2D and update animation tracks/paths

--- objects/crate/Crate.cs
+++ objects/crate/Crate.cs
@@ -3,7 +3,7 @@ using Pigdom.Recipes;
 
 namespace Pigdom.Objects;
 
-public partial class Crate : Node2D
+public partial class Crate : RigidBody2D
 {
     [Signal]
     public delegate void BrokeEventHandler();
@@ -12,31 +12,30 @@ public partial class Crate : Node2D
     public int Health { get; set; } = 3;
 
     [Export]
-    public float Gravity { get; set; } = 4000f;
+    public float ShatterVelocityThreshold { get; set; } = 300f;
 
     private AnimationPlayer _animationPlayer;
-    private CharacterBody2D _body;
-    private RayCast2D _ray;
     private HurtArea2D _hurtBox2D;
+    private CollisionShape2D _collisionShape;
 
     public override void _Ready()
     {
         _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-        _body = GetNode<CharacterBody2D>("CharacterBody2D");
-        _ray = GetNode<RayCast2D>("CharacterBody2D/RayCast2D");
-        _hurtBox2D = GetNode<HurtArea2D>("CharacterBody2D/HurtBox2D");
+        _hurtBox2D = GetNode<HurtArea2D>("HurtBox2D");
+        _collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
 
         _hurtBox2D.Hurt += OnHurtBox2DHurt;
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        if (!_ray.IsColliding())
-        {
-            _body.Velocity = _body.Velocity with { Y = _body.Velocity.Y + Gravity * (float)delta };
-        }
+        bool shouldFreeze = Sleeping;
 
-        _body.MoveAndSlide();
+        if (Freeze != shouldFreeze)
+        {
+            Freeze = shouldFreeze;
+            _collisionShape.OneWayCollision = shouldFreeze;
+        }
     }
 
     private void Damage(int damage)
@@ -55,8 +54,21 @@ public partial class Crate : Node2D
 
     protected virtual void Shatter()
     {
+        WakeUpNearbyCrates();
         EmitSignal(SignalName.Broke);
         _animationPlayer.Play("break");
+    }
+
+    private void WakeUpNearbyCrates()
+    {
+        // Wake up any crates that are currently colliding with this one
+        foreach (var body in GetCollidingBodies())
+        {
+            if (body is Crate crate)
+            {
+                crate.Sleeping = false;
+            }
+        }
     }
 
     private void PlayHitAnimation() => _animationPlayer.Play("hit");
